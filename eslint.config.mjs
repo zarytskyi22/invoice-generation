bd8ce3e1c1c541e04a3fb420683aba0ba76ed247
Yurii <yurii.z@overcode.tech>
2025-05-02 12:39:36 +0300

    Add project structure

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,6 @@
+import { FlatCompat } from "@eslint/eslintrc";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -10,7 +10,23 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends(
+    "next/core-web-vitals",
+    "next/typescript",
+    "plugin:prettier/recommended"
+  ),
+  {
+    rules: {
+      allowObjectTypes: "off",
+      "no-console": ["warn", { allow: ["warn", "error"] }],
+      "prettier/prettier": ["warn", { endOfLine: "auto" }],
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-unused-vars": "off",
+      "@typescript-eslint/no-empty-object-type": "off",
+      "no-empty-pattern": "off",
+      "no-extra-boolean-cast": "off",
+    },
+  },
 ];
 
 export default eslintConfig;
