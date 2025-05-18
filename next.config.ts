a1f21342af70172e7426a0e394d5e2663ececea9
Yurii <yurii.z@overcode.tech>
2025-05-18 13:06:19 +0300

    Setup Supabase + Next.js config

--- next.config.ts
+++ next.config.ts
@@ -1,46 +1,7 @@
-import type { NextConfig } from 'next';
-import path from 'node:path';
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  reactStrictMode: true,
-
-  sassOptions: {
-    includePaths: [path.join(process.cwd(), 'styles')],
-  },
-
-  images: {
-    remotePatterns: [
-      // {
-      //   protocol: 'https',
-      //   hostname: 'green-creepy-cockroach-553.mypinata.cloud',
-      //   pathname: '**',
-      // },
-    ],
-  },
-
-  webpack: (config) => {
-    const fileLoaderRule = config.module.rules.find((rule: any) => rule.test?.test?.('.svg'));
-
-    config.module.rules.push(
-      {
-        ...fileLoaderRule,
-        test: /\.svg$/i,
-        resourceQuery: /url/,
-      },
-      {
-        test: /\.svg$/i,
-        issuer: fileLoaderRule.issuer,
-        resourceQuery: { not: [...fileLoaderRule.resourceQuery.not, /url/] },
-        use: ['@svgr/webpack'],
-      }
-    );
-    fileLoaderRule.exclude = /\.svg$/i;
-
-    config.resolve.fallback = { fs: false, net: false, tls: false };
-    config.externals.push('pino-pretty', 'lokijs', 'encoding');
-
-    return config;
-  },
+  /* config options here */
 };
 
 export default nextConfig;
--- src/app/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <main className="flex min-h-[100dvh] items-center justify-center font-sans">
-      <h1 className="text-5xl font-medium">Invoice generator </h1>
-    </main>
-  );
-}
